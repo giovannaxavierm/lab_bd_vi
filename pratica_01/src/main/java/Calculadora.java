0e5b35d1731ff5123064bcb8157a6066f0140515
giovannaxavier <giovanna-xavierm@hotmail.com>
2021-03-26 20:34:24 -0300

    Configuração para utilizar o gradle run

--- pratica_01/src/main/java/Calculadora.java
+++ pratica_01/src/main/java/Calculadora.java
@@ -1,6 +1,7 @@
 public class Calculadora {
     // fase azul
     // Método refatorado e melhorado utilizando a lib math
+    public static void main(String[] args) {}
     public double raiz(double n1){
         return(Math.sqrt(n1));
     }
