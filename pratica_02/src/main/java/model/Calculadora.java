c072bd6d0165c9afef950032b2845438fe143d6a
giovannaxavier <giovanna-xavierm@hotmail.com>
2021-06-10 20:14:14 -0300

    Prática 02 - Fase Azul

--- pratica_02/src/main/java/model/Calculadora.java
+++ pratica_02/src/main/java/model/Calculadora.java
@@ -2,8 +2,7 @@ package model;
 
 public class Calculadora {
     public double raizQuadrada(double n1) {
-        //Método suficiente para passar na fase verde, apenas o negativo que falha
-        double result = Math.sqrt(n1);
-        return result;
+        //Utilizando a lib math direto no return
+        return(Math.sqrt(n1));
     }
 }
