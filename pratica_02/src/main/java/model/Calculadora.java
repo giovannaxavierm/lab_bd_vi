68fcff6a455dd361bc2112bc71a2bba8491f222a
giovannaxavier <giovanna-xavierm@hotmail.com>
2021-06-21 21:35:10 -0300

    Correção - Pratica 02

--- pratica_02/src/main/java/model/Calculadora.java
+++ pratica_02/src/main/java/model/Calculadora.java
@@ -3,6 +3,10 @@ package model;
 public class Calculadora {
     public double raizQuadrada(double n1) {
         //Utilizando a lib math direto no return
+        // validando o n1 caso for negativo
+        if(n1 < 0) {
+            return -1;
+        }
         return(Math.sqrt(n1));
     }
 }
